28ca47e89099f4724ed0a06e7295aa612af5a877
Luo Yu <hl1282456555@outlook.com>
2021-12-08 17:54:30 +0800

    upload project

---
+++ AutoKey/AutoKey.cpp
@@ -0,0 +1,111 @@
+// AutoKey.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+
+#include "Utils.h"
+#include "json.hpp"
+
+int main()
+{
+    std::cout << "Welcome auto key!" << std::endl;
+    std::cout << "Loading the config file." << std::endl;
+
+    using Json = nlohmann::json;
+
+    std::ifstream ConfigFile("Config.json");
+    if (!ConfigFile.is_open()) {
+        std::cout << "Cannot open the config file." << std::endl;
+        return -1;
+    }
+
+    int ControlKeyCode;
+    bool bShouldLoop = false;
+    std::vector<FCommandInfo> Commands;
+    try
+    {
+		Json ConfigJson;
+		ConfigFile >> ConfigJson;
+
+        if (ConfigJson.find("loop") != ConfigJson.end()) {
+            bShouldLoop = ConfigJson["loop"];
+        }
+
+        std::cout << "Loop flag is set to " << (bShouldLoop ? "true." : "false.") << std::endl;
+
+		std::string ControlKeyStr = ConfigJson["controlKey"];
+        if (!MapKeyToCode(ControlKeyStr, ControlKeyCode)) {
+            std::cout << "'controlKey' is not in right range, please fix it." << std::endl;
+            return -1;
+        }
+
+        std::cout << "Set the '" << ControlKeyStr << "' key to control the processor." << std::endl;
+
+        std::cout << "Parsing the command list." << std::endl;
+
+        std::vector<Json> CommandListArray = ConfigJson["commandList"];
+        if (CommandListArray.empty()) {
+            std::cout << "Cannot found any command, please enter your command list, e.g: {\"key\": \"1\", \"waitTime\": 1000}." << std::endl;
+            return -1;
+        }
+
+        for (auto CommandJson : CommandListArray)
+        {
+            FCommandInfo NewCommand;
+            if (!MapKeyToCode(CommandJson["key"], NewCommand.KeyCode)) {
+                std::cout << CommandJson["key"] << " is not available key." << std::endl;
+                return -1;
+            }
+
+            NewCommand.WaitTime = CommandJson["waitTime"];
+
+            Commands.push_back(std::move(NewCommand));
+        }
+    }
+    catch (const std::exception& Error)
+    {
+        std::cout << "Fatal error occurred: %s" << Error.what() << std::endl;
+        return false;
+    }
+
+    bool bStartProcess = false;
+    int NextCommandIndex = 0;
+
+    std::cout << "Start to process the command list, you can enter the control key to stop it." << std::endl;
+    while (1)
+    {
+       bool bControlKeyPressed = GetAsyncKeyState(ControlKeyCode) & 0x01;
+
+       if (bControlKeyPressed) {
+           bStartProcess = !bStartProcess;
+           std::cout << "Control key pressed, will " << (bStartProcess ? "wakeup" : "stop") << " the processor." << std::endl;
+       }
+
+        if (!bStartProcess) {
+			Sleep(1000);
+			NextCommandIndex = 0;
+            continue;
+        }
+
+        const FCommandInfo Command = Commands[NextCommandIndex];
+
+        keybd_event(Command.KeyCode, MapVirtualKey(Command.KeyCode, MAPVK_VK_TO_VSC), KEYEVENTF_EXTENDEDKEY | 0, 0);
+        keybd_event(Command.KeyCode, MapVirtualKey(Command.KeyCode, MAPVK_VK_TO_VSC), KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+
+        Sleep(Command.WaitTime);
+
+        NextCommandIndex++;
+        if (NextCommandIndex >= Commands.size()) {
+            std::cout << "All commands in the list is finished." << std::endl;
+            NextCommandIndex = 0;
+
+            if (!bShouldLoop) {
+                bStartProcess = false;
+                continue;
+            }
+        }
+    }
+}
---
+++ AutoKey/Utils.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <chrono>
+#include <algorithm>
+
+#include <WinUser.h>
+
+struct FCommandInfo
+{
+	int KeyCode;
+	int WaitTime;
+};
+
+static std::map<std::string, int> ControlKeyMap = {
+	{"F1", VK_F1}, {"F2", VK_F2}, {"F3", VK_F3}, {"F4", VK_F4}, {"F5", VK_F5}, {"F6", VK_F6}, {"F7", VK_F7}, {"F8", VK_F8}, {"F9", VK_F9}, {"F10", VK_F10}, {"F11", VK_F11}, {"F12", VK_F12},
+	{"0", 0x30}, {"1", 0x31}, {"2", 0x32}, {"3", 0x33}, {"4", 0x34}, {"5", 0x35}, {"6", 0x36}, {"7", 0x37}, {"8", 0x38}, {"9", 0x39},
+	{"A", 0x41}, {"B", 0x42}, {"C", 0x43}, {"D", 0x44}, {"E", 0x45}, {"F", 0x46}, {"G", 0x47}, {"H", 0x48}, {"I", 0x49}, {"J", 0x4A}, {"K", 0x4B}, {"L", 0x4C},
+	{"M", 0x4D}, {"N", 0x4E}, {"O", 0x4F}, {"P", 0x50}, {"Q", 0x51}, {"R", 0x52}, {"S", 0x53}, {"T", 0x54}, {"U", 0x55}, {"V", 0x56}, {"W", 0x57}, {"X", 0x58},
+	{"Y", 0x59}, {"Z", 0x5A}, 
+	{"N0", VK_NUMPAD0}, {"N1", VK_NUMPAD1}, {"N2", VK_NUMPAD2}, {"N3", VK_NUMPAD3}, {"N4", VK_NUMPAD4}, {"N5", VK_NUMPAD5}, {"N6", VK_NUMPAD6}, {"N7", VK_NUMPAD7},
+	{"N8", VK_NUMPAD8}, {"N9", VK_NUMPAD9},
+	{"Space", VK_SPACE}, {"Back", VK_BACK}, {"\\", VK_OEM_5}, {"~", VK_OEM_3}, {"Enter", VK_RETURN}, {"Ins", VK_INSERT}, {"Home", VK_HOME}, {"PageUp", VK_PRIOR},
+	{"PageDown", VK_NEXT}, {"Delete", VK_DELETE}, {"End", VK_END}, {"+", VK_OEM_PLUS}, {"-", VK_OEM_MINUS}, {",", VK_OEM_COMMA}, {".", VK_OEM_PERIOD}
+};
+
+bool MapKeyToCode(const std::string& InKey, int& OutCode)
+{
+	std::string tempKey(InKey);
+	std::transform(InKey.begin(), InKey.end(), tempKey.begin(), std::toupper);
+
+	auto FoundIt = std::find_if(ControlKeyMap.begin(), ControlKeyMap.end(), [&](const std::pair<std::string, int>& Value)
+		{ 
+			if (InKey.size() != Value.first.size()) {
+				return false;
+			}
+
+			return Value.first.compare(tempKey) == 0;
+		});
+
+	if (FoundIt == ControlKeyMap.end()) {
+		return false;
+	}
+
+	OutCode = FoundIt->second;
+
+	return true;
+}
